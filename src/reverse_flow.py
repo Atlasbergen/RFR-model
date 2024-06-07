dfc3a047bee72d4673e7792e3033210d35a5f958
Jesper Carlsson <atlasbergen@proton.me>
2024-06-07 07:20:55 +0200

    Forgot to add the actual file ...

---
+++ src/reverse_flow.py
@@ -0,0 +1,279 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+import time as tm
+from classes import *
+from functions import *
+
+CH3OH, O2, HCHO, H2O, CO, DME, DMM, N2 = [
+    Molecule("Methanol", Mw_Me, H_f_Me, Tb_Me, Vb_Me, Param_Mu_Me, Param_Cp_Me, Param_kappa_Me,),
+    Molecule("Oxygen",  Mw_O2, H_f_O2,  Tb_O2, Vb_O2, Param_Mu_O2, Param_Cp_O2, Param_kappa_O2),
+    Molecule("Formaldehyde", Mw_HCHO, H_f_HCHO, Tb_HCHO, Vb_HCHO, Param_Mu_HCHO, Param_Cp_HCHO, Param_kappa_HCHO,),
+    Molecule("Water", Mw_H2O, H_f_H2O, Tb_H2O, Vb_H2O, Param_Mu_H2O, Param_Cp_H2O, Param_kappa_H2O,),
+    Molecule("Carbon Monoxide", Mw_CO, H_f_CO, Tb_CO, Vb_CO, Param_Mu_CO, Param_Cp_CO, Param_kappa_CO,),
+    Molecule("DME", Mw_DME, H_f_DME, Tb_DME, Vb_DME, Param_Mu_DME, Param_Cp_DME, Param_kappa_DME,),
+    Molecule("DMM", Mw_DMM, H_f_DMM, Tb_DMM, Vb_DMM, Param_Mu_DMM, Param_Cp_DMM, Param_kappa_DMM,),
+    Molecule("Nitrogen", Mw_N2, H_f_N2, Tb_N2, Vb_N2, Param_Mu_N2, Param_Cp_N2, Param_kappa_N2,),
+]
+
+r1, r2, r3, r4, r5 = [
+    Reaction("reaction_1", [1, 0.5], [1, 1], [CH3OH, O2], [HCHO, H2O]),
+    Reaction("reaction_2", [1, 0.5], [1, 1], [HCHO, O2], [CO, H2O]),
+    Reaction("reaction_3", [2], [1, 1], [CH3OH], [DME, H2O]),
+    Reaction("reaction_4", [2, 1], [1, 1], [CH3OH, HCHO], [DMM, H2O]),
+    Reaction("reaction_5", [1, 1], [2, 1], [DME, O2], [HCHO, H2O]),
+]
+
+rho_cat_p = rho_cat * (1-porosity(2*r_inner, 2*r_part))
+AC = a_c(2 * r_inner, 2 * r_part)
+part_dia = r_part * 2
+eps_fac = (1-porosity(2*r_inner, 2*r_part))/porosity(2*r_inner, 2*r_part)
+eps_fac_2 = porosity(2*r_inner, 2*r_part)/(1-porosity(2*r_inner, 2*r_part))
+
+m = 100
+snaps = 100
+t_dur = 30
+
+length = reactor_len(w_cat)
+dx = length / m
+
+num_vars = 16 
+entries = num_vars*m
+
+def deriv(t, C):
+    # dCdt = np.zeros_like(C)
+    dCdt = [0] * entries
+
+    for i in range(1, m):
+        
+        C_tot = C[i] + C[i + m] + C[i + 2*m] + C[i + 3*m] + C[i + 4*m] + C[i + 5*m] + C[i + 6*m] + C_I0
+        u_all = u(C[i + 7*m], P_0, C_tot, C[i], C[i + m], C[i + 2*m], C[i + 3*m], C[i + 4*m], C[i + 5*m], C[i + 6*m], C_I0)
+        rho_gas_mix = rho_mix(C[i + 7*m], P_0, C_tot, C[i], C[i + m], C[i + 2*m], C[i + 3*m], C[i + 4*m], C[i + 5*m], C[i + 6*m], C_I0)
+        mu_gas_all = mu_gas_mix(C[i + 7*m], C_tot, [C[i], C[i + m], C[i + 2*m], C[i + 3*m], C[i + 4*m], C[i + 5*m], C[i + 6*m], C_I0], [CH3OH, O2, HCHO, H2O, CO, DME, DMM, N2])
+        kappa_gas = kappa_gas_mix(C[i + 7*m], C_tot, [C[i], C[i + m], C[i + 2*m], C[i + 3*m], C[i + 4*m], C[i + 5*m], C[i + 6*m], C_I0], [CH3OH, O2, HCHO, H2O, CO, DME, DMM, N2])
+        Cp_gas = Cp_gas_mix(C[i + 7*m], C_tot, [C[i], C[i + m], C[i + 2*m], C[i + 3*m], C[i + 4*m], C[i + 5*m], C[i + 6*m], C_I0], [CH3OH, O2, HCHO, H2O, CO, DME, DMM, N2])
+
+        r_1_all = r1.r(C[i + 15*m], C[i + 8*m], C[i + 9*m], C[i + 10*m], C[i + 11*m], C[i + 12*m], C[i + 13*m], C[i + 14*m])
+        r_2_all = r2.r(C[i + 15*m], C[i + 8*m], C[i + 9*m], C[i + 10*m], C[i + 11*m], C[i + 12*m], C[i + 13*m], C[i + 14*m])
+        r_3_all = r3.r(C[i + 15*m], C[i + 8*m], C[i + 9*m], C[i + 10*m], C[i + 11*m], C[i + 12*m], C[i + 13*m], C[i + 14*m])
+        r_4_all = r4.r(C[i + 15*m], C[i + 8*m], C[i + 9*m], C[i + 10*m], C[i + 11*m], C[i + 12*m], C[i + 13*m], C[i + 14*m])
+        r_5_all = r5.r(C[i + 15*m], C[i + 8*m], C[i + 9*m], C[i + 10*m], C[i + 11*m], C[i + 12*m], C[i + 13*m], C[i + 14*m])
+
+
+        dCdt[i] = -u_all * ((C[i] - C[i-1]) / (dx)) + k_c(
+            rho_gas_mix,
+            mu_gas_all,
+            u_all,
+            part_dia,
+            CH3OH.D_eff(C[i + 7*m], P_0, C_tot, C[i], [C[i + m], C[i + 2*m], C[i + 3*m], C[i + 4*m], C[i + 5*m], C[i + 6*m], C_I0], [O2, HCHO, H2O, CO, DME, DMM, N2]),
+        ) * AC * (C[i + 8*m] - C[i])
+
+        dCdt[i + m] = -u_all * ((C[i + m] - C[i-1 + m]) / (dx)) + k_c(
+            rho_gas_mix,
+            mu_gas_all,
+            u_all,
+            part_dia,
+            O2.D_eff(C[i + 7*m], P_0, C_tot, C[i + m], [C[i], C[i + 2*m], C[i + 3*m], C[i + 4*m], C[i + 5*m], C[i + 6*m], C_I0], [CH3OH, HCHO, H2O, CO, DME, DMM, N2]),
+        ) * AC * (C[i + 9*m] - C[i + m])
+
+        dCdt[i + 2*m] = -u_all * ((C[i + 2*m] - C[i-1 + 2*m]) / (dx)) + k_c(
+            rho_gas_mix,
+            mu_gas_all,
+            u_all,
+            part_dia,
+            HCHO.D_eff(C[i + 7*m], P_0, C_tot, C[i + 2*m], [C[i + m], C[i], C[i + 3*m], C[i + 4*m], C[i + 5*m], C[i + 6*m], C_I0], [O2, CH3OH, H2O, CO, DME, DMM, N2]),
+        ) * AC * (C[i + 10*m] - C[i + 2*m])
+
+        dCdt[i + 3*m] = -u_all * ((C[i + 3*m] - C[i-1 + 3*m]) / (dx)) + k_c(
+            rho_gas_mix,
+            mu_gas_all,
+            u_all,
+            part_dia,
+            H2O.D_eff(C[i + 7*m], P_0, C_tot, C[i + 3*m], [C[i + m], C[i + 2*m], C[i], C[i + 4*m], C[i + 5*m], C[i + 6*m], C_I0], [O2, HCHO, CH3OH, CO, DME, DMM, N2]),
+        ) * AC * (C[i + 11*m] - C[i + 3*m])
+        
+        dCdt[i + 4*m] = -u_all * ((C[i + 4*m] - C[i-1 + 4*m]) / (dx)) + k_c(
+            rho_gas_mix,
+            mu_gas_all,
+            u_all,
+            part_dia,
+            CO.D_eff(C[i + 7*m], P_0, C_tot, C[i + 4*m], [C[i], C[i + m], C[i + 2*m], C[i + 3*m], C[i + 5*m], C[i + 6*m], C_I0], [CH3OH, O2, HCHO, H2O, DME, DMM, N2]),
+        ) * AC * (C[i + 12*m] - C[i + 4*m])
+
+        dCdt[i + 5*m] = -u_all * ((C[i + 5*m] - C[i-1 + 5*m]) / (dx)) + k_c(
+            rho_gas_mix,
+            mu_gas_all,
+            u_all,
+            part_dia,
+            DME.D_eff(C[i + 7*m], P_0, C_tot, C[i + 5*m], [C[i], C[i + m], C[i + 2*m], C[i + 3*m], C[i + 4*m], C[i + 6*m], C_I0], [CH3OH, O2, HCHO, H2O, CO, DMM, N2]),
+        ) * AC * (C[i + 13*m] - C[i + 5*m])
+        
+        dCdt[i + 6*m] = -u_all * ((C[i + 6*m] - C[i-1 + 6*m]) / (dx)) + k_c(
+            rho_gas_mix,
+            mu_gas_all,
+            u_all,
+            part_dia,
+            DMM.D_eff(C[i + 7*m], P_0, C_tot, C[i + 6*m], [C[i], C[i + m], C[i + 2*m], C[i + 3*m], C[i + 4*m], C[i + 5*m], C_I0], [CH3OH, O2, HCHO, H2O, CO, DME, N2]),
+        ) * AC * (C[i + 14*m] - C[i + 6*m])
+
+        dCdt[i + 7*m] = -u_all * ((C[i + 7*m] - C[i-1 + 7*m]) / (dx)) + h(
+            rho_gas_mix,
+            mu_gas_all,
+            u_all,
+            part_dia,
+            kappa_gas,
+            Cp_gas
+        ) * AC * (1/(CH3OH.Cp(C[i + 7*m])*C[i] + O2.Cp(C[i + 7*m])*C[i + m] + HCHO.Cp(C[i + 7*m])*C[i + 2*m] + H2O.Cp(C[i + 7*m])*C[i + 3*m] + CO.Cp(C[i + 7*m])*C[i + 4*m] + DME.Cp(C[i + 7*m])*C[i + 5*m] + DMM.Cp(C[i + 7*m])*C[i + 6*m] + N2.Cp(C[i + 7*m])*C_I0)) * (C[i + 15*m] - C[i + 7*m])
+        
+        dCdt[i + 8*m] = k_c(
+        rho_gas_mix,
+        mu_gas_all,
+        u_all,
+        part_dia,
+        CH3OH.D_eff(C[i + 7*m], P_0, C_tot, C[i], [C[i + m], C[i + 2*m], C[i + 3*m], C[i + 4*m], C[i + 5*m], C[i + 6*m], C_I0], [O2, HCHO, H2O, CO, DME, DMM, N2]),
+        ) * AC * (C[i] - C[i + 8*m]) - rho_cat_p*eps_fac*(r_1_all + 2*r_3_all + 2*r_4_all)
+
+        dCdt[i + 9*m] = k_c(
+        rho_gas_mix,
+        mu_gas_all,
+        u_all,
+        part_dia,
+        O2.D_eff(C[i + 7*m], P_0, C_tot, C[i + m], [C[i], C[i + 2*m], C[i + 3*m], C[i + 4*m], C[i + 5*m], C[i + 6*m], C_I0], [CH3OH, HCHO, H2O, CO, DME, DMM, N2]),
+        ) * AC * (C[i + m] - C[i + 9*m]) - 0.5*rho_cat_p*eps_fac*(r_1_all + r_2_all + r_5_all)
+
+        dCdt[i + 10*m] = k_c(
+        rho_gas_mix,
+        mu_gas_all,
+        u_all,
+        part_dia,
+        HCHO.D_eff(C[i + 7*m], P_0, C_tot, C[i + 2*m], [C[i + m], C[i], C[i + 3*m], C[i + 4*m], C[i + 5*m], C[i + 6*m], C_I0], [O2, CH3OH, H2O, CO, DME, DMM, N2]),
+        ) * AC * (C[i + 2*m] - C[i + 10*m]) + rho_cat_p*eps_fac*((r_1_all + r_5_all) - (r_2_all + r_4_all))
+
+        dCdt[i + 11*m] = k_c(
+        rho_gas_mix,
+        mu_gas_all,
+        u_all,
+        part_dia,
+        H2O.D_eff(C[i + 7*m], P_0, C_tot, C[i + 3*m], [C[i + m], C[i + 2*m], C[i], C[i + 4*m], C[i + 5*m], C[i + 6*m], C_I0], [O2, HCHO, CH3OH, CO, DME, DMM, N2]),
+        ) * AC * (C[i + 3*m] - C[i + 11*m]) + rho_cat_p*eps_fac*(r_1_all + r_2_all + r_3_all + r_4_all + 0.5*r_5_all)
+
+        dCdt[i + 12*m] = k_c(
+        rho_gas_mix,
+        mu_gas_all,
+        u_all,
+        part_dia,
+        CO.D_eff(C[i + 7*m], P_0, C_tot, C[i + 4*m], [C[i], C[i + m], C[i + 2*m], C[i + 3*m], C[i + 5*m], C[i + 6*m], C_I0], [CH3OH, O2, HCHO, H2O, DME, DMM, N2]),
+    ) * AC * (C[i + 4*m] - C[i + 12*m]) + rho_cat_p*eps_fac*r_2_all
+
+        dCdt[i + 13*m] = k_c(
+        rho_gas_mix,
+        mu_gas_all,
+        u_all,
+        part_dia,
+        DME.D_eff(C[i + 7*m], P_0, C_tot, C[i + 5*m], [C[i], C[i + m], C[i + 2*m], C[i + 3*m], C[i + 4*m], C[i + 6*m], C_I0], [CH3OH, O2, HCHO, H2O, CO, DMM, N2]),
+        ) * AC * (C[i + 5*m] - C[i + 13*m]) + rho_cat_p*eps_fac*(r_3_all - 0.5*r_5_all)
+        
+        dCdt[i + 14*m] = k_c(
+        rho_gas_mix,
+        mu_gas_all,
+        u_all,
+        part_dia,
+        DMM.D_eff(C[i + 7*m], P_0, C_tot, C[i + 6*m], [C[i], C[i + m], C[i + 2*m], C[i + 3*m], C[i + 4*m], C[i + 5*m], C_I0], [CH3OH, O2, HCHO, H2O, CO, DME, N2]),
+        ) * AC * (C[i + 6*m] - C[i + 14*m]) + rho_cat_p*eps_fac*r_4_all
+        
+        if i != m-1:
+            dCdt[i + 15*m] = (1/(rho_cat_p * 374 * 0.8))*(((C[i + 15*m + 1] - 2*C[i + 15*m] + C[i + 15*m - 1])/dx**2) + eps_fac_2*h(
+                rho_gas_mix,
+                mu_gas_all,
+                u_all,
+                part_dia,
+                kappa_gas,
+                Cp_gas
+            ) * AC * (C[i + 7*m] - C[i + 15*m]) + (rho_cat_p*((-r_1_all*r1.H_rxn(C[i + 15*m])) + (-r_2_all*r2.H_rxn(C[i + 15*m])) + (-r_3_all*r3.H_rxn(C[i + 15*m])) + (-r_4_all*r4.H_rxn(C[i + 15*m])) + (-r_5_all*r5.H_rxn(C[i + 15*m])))))
+        else:
+            dCdt[i + 15*m] = (1/(rho_cat_p * 374 * 0.8))*(eps_fac_2*h(
+                rho_gas_mix,
+                mu_gas_all,
+                u_all,
+                part_dia,
+                kappa_gas,
+                Cp_gas
+            ) * AC * (C[i + 7*m] - C[i + 15*m]) + (rho_cat_p*((-r_1_all*r1.H_rxn(C[i + 15*m])) + (-r_2_all*r2.H_rxn(C[i + 15*m])) + (-r_3_all*r3.H_rxn(C[i + 15*m])) + (-r_4_all*r4.H_rxn(C[i + 15*m])) + (-r_5_all*r5.H_rxn(C[i + 15*m])))))
+
+    return dCdt
+
+
+uinit = np.zeros(num_vars*m)
+uinit[0] = C_A0
+uinit[m] = C_B0
+uinit[8*m:9*m] = 1e-10
+uinit[m*7:m*8] = T_0
+uinit[m*15:m*16] = T_0
+
+time = np.linspace(0, t_dur, snaps)
+
+start = tm.time()
+for i in range(4):
+    sol = solve_ivp(deriv, (0, t_dur), uinit, method='RK23', t_eval=time, atol=1e-6, rtol=1e-6)
+    
+    C = sol.y[:, -1]
+
+    C_A = C[:m]
+    C_B = C[m:2*m]
+    C_C = C[2*m:3*m]
+    C_D = C[3*m:4*m]
+    C_E = C[4*m:5*m]
+    C_F = C[5*m:6*m]
+    C_G = C[6*m:7*m]
+    T_b = C[7*m:8*m]
+    C_As = C[8*m:9*m]
+    C_Bs = C[9*m:10*m]
+    C_Cs = C[10*m:11*m]
+    C_Ds = C[11*m:12*m]
+    C_Es = C[12*m:13*m]
+    C_Fs = C[13*m:14*m]
+    C_Gs = C[14*m:15*m]
+    T_s = C[15*m:16*m]
+    
+    uinit = np.zeros(num_vars*m)
+    uinit[:m] = C_A[::-1]
+    uinit[m:2*m] = C_B[::-1]
+    uinit[2*m:3*m] = C_C[::-1]
+    uinit[3*m:4*m] = C_D[::-1]
+    uinit[4*m:5*m] = C_E[::-1]
+    uinit[5*m:6*m] = C_F[::-1]
+    uinit[6*m:7*m] = C_G[::-1]
+    uinit[7*m:8*m] = T_b[::-1]
+    uinit[8*m:9*m] = C_As[::-1]
+    uinit[9*m:10*m] = C_Bs[::-1]
+    uinit[10*m:11*m] = C_Cs[::-1]
+    uinit[11*m:12*m] = C_Ds[::-1]
+    uinit[12*m:13*m] = C_Es[::-1]
+    uinit[13*m:14*m] = C_Fs[::-1]
+    uinit[14*m:15*m] = C_Gs[::-1]
+    uinit[15*m:16*m] = T_s[::-1]
+
+    uinit[0] = C_A0
+    uinit[m] = C_B0
+    uinit[2*m] = 0
+    uinit[3*m] = 0
+    uinit[4*m] = 0
+    uinit[5*m] = 0
+    uinit[6*m] = 0
+    uinit[7*m] = T_0
+
+
+stop = tm.time()
+
+print(stop - start)
+
+# x_points = np.linspace(0, length, m)
+#
+# plt.plot(x_points, sol.y[:m, -1], x_points, sol.y[m:2*m, -1], x_points, sol.y[2*m:3*m, -1], x_points, sol.y[3*m:4*m, -1], x_points, sol.y[4*m:5*m, -1], x_points, sol.y[5*m:6*m, -1], x_points, sol.y[6*m:7*m, -1])
+# plt.show()
+#
+# plt.plot(x_points, sol.y[7*m:8*m, -1], x_points, sol.y[15*m:16*m, -1])
+# plt.show()
+
+np.savetxt("data/output_rev_data.txt", sol.y)
